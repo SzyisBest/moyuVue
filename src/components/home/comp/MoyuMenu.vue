eb905bcb36c5702245f55aac0174fd9cc08fe6b1
1476759792 <1476759792@qq.com>
2022-09-18 18:15:35 +0800

    主页修改

--- src/components/home/comp/MoyuMenu.vue
+++ src/components/home/comp/MoyuMenu.vue
@@ -2,73 +2,122 @@
   <div>
     <el-row>
       <el-col :span="24">
-        <el-menu
-            id="menu-style"
-            default-active="1"
-            class="el-menu-vertical-demo menu-text"
-            mode="vertical"
-            :ellipsis="true"
-            background-color="#b94a3e"
-            :router="true"
-            text-color="#fff"
-        >
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon>
-                <Menu color="#fff"></Menu>
-              </el-icon>
-              <span class="menu-text">菜单设置</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="1-1">菜单设置</el-menu-item>
-              <el-menu-item index="1-2">菜单设置</el-menu-item>
-            </el-menu-item-group>
-          </el-sub-menu>
-          <el-sub-menu index="2">
-            <template #title>
-              <el-icon>
-                <Reading color="#fff"/>
-              </el-icon>
-              <span>墨鱼课程</span>
-            </template>
+        <el-scrollbar>
+          <el-menu
+              id="menu-style"
+              default-active="1"
+              class="el-menu-vertical-demo menu-text"
+              mode="vertical"
+              :ellipsis="true"
+              background-color="#b94a3e"
+              :router="true"
+              text-color="#fff"
+          >
+            <el-sub-menu index="1">
+              <template #title>
+                <el-icon>
+                  <Menu color="#fff"></Menu>
+                </el-icon>
+                <span class="menu-text">菜单设置</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="1-1">
+                  <template #title>
+                    <el-icon>
+                      <Memo/>
+                    </el-icon>
+                    <span class="menu-text">字典管理</span>
+                  </template>
+                </el-menu-item>
+                <el-menu-item index="1-2">
+                  <template #title>
+                    <el-icon>
+                      <Lock/>
+                    </el-icon>
+                    <span class="menu-text">权限设置</span>
+                  </template>
+                </el-menu-item>
+                <el-menu-item index="1-3">
+                  <template #title>
+                    <el-icon>
+                      <User/>
+                    </el-icon>
+                    <span class="menu-text">角色管理</span>
+                  </template>
+                </el-menu-item>
+                <el-menu-item index="1-4">
+                  <template #title>
+                    <el-icon>
+                      <Menu/>
+                    </el-icon>
+                    <span class="menu-text">菜单管理</span>
+                  </template>
+                </el-menu-item>
+              </el-menu-item-group>
+            </el-sub-menu>
+            <el-sub-menu index="2">
+              <template #title>
+                <el-icon>
+                  <Reading color="#fff"/>
+                </el-icon>
+                <span>墨鱼课程</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="1-1">
+                  <template #title>
+                    <el-icon>
+                      <Memo/>
+                    </el-icon>
+                    <span class="menu-text">学生课表</span>
+                  </template>
+                </el-menu-item>
+                <el-menu-item index="1-2">
+                  <template #title>
+                    <el-icon>
+                      <Notebook/>
+                    </el-icon>
+                    <span class="menu-text">课表导入</span>
+                  </template>
+                </el-menu-item>
+              </el-menu-item-group>
+            </el-sub-menu>
+            <el-sub-menu index="3">
+              <template #title>
+                <el-icon>
+                  <BellFilled color="#fff"/>
+                </el-icon>
+                <span>健康上报</span>
+              </template>
 
-          </el-sub-menu>
-          <el-sub-menu index="3">
-            <template #title>
-              <el-icon>
-                <BellFilled color="#fff"/>
-              </el-icon>
-              <span>健康上报</span>
-            </template>
+            </el-sub-menu>
+            <el-sub-menu index="4">
+              <template #title>
+                <el-icon>
+                  <Comment color="#fff"/>
+                </el-icon>
+                <span>申请列表</span>
+              </template>
 
-          </el-sub-menu>
-          <el-sub-menu index="4">
-            <template #title>
-              <el-icon>
-                <Comment color="#fff"/>
-              </el-icon>
-              <span>申请列表</span>
-            </template>
+            </el-sub-menu>
+            <el-sub-menu index="5">
+              <template #title>
+                <el-icon>
+                  <CircleCheck color="#fff"/>
+                </el-icon>
+                <span>评教打分</span>
+              </template>
 
-          </el-sub-menu>
-          <el-sub-menu index="5">
-            <template #title>
-              <el-icon>
-                <CircleCheck color="#fff"/>
-              </el-icon>
-              <span>评教打分</span>
-            </template>
-
-          </el-sub-menu>
-          <el-sub-menu index="6">
-            <template #title>
-              <el-icon>
-                <DataLine color="#fff"/>
-              </el-icon>
-              <span>任务清单</span>
-            </template>
-          </el-sub-menu>
-        </el-menu>
+            </el-sub-menu>
+            <el-sub-menu index="6">
+              <template #title>
+                <el-icon>
+                  <DataLine color="#fff"/>
+                </el-icon>
+                <span>任务清单</span>
+              </template>
+            </el-sub-menu>
+          </el-menu>
+        </el-scrollbar>
       </el-col>
     </el-row>
   </div>
@@ -90,6 +139,7 @@ export default {
 <style scoped>
 .el-menu {
   height: 100vh;
+  user-select: none;
 }
 
 </style>
